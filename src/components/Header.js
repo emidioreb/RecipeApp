e0d3dcfb33e821129024f215e8564fe1ba405cde
Leomar Linhares <leomar_linhares@hotmail.com>
2021-11-08 17:07:29 -0300

    impl: add image to header buttons

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,13 @@ import React from 'react';
 export default function Header() {
   return (
     <header>
-      <button type="button" data-testid="profile-top-btn">Perfil</button>
+      <button type="button" data-testid="profile-top-btn">
+        <img src="../images/profileIcon.svg" alt="Profile icon" />
+      </button>
       <h1 data-testid="page-title">Título</h1>
-      <button type="button" data-testid="search-top-btn">Buscar</button>
+      <button type="button" data-testid="search-top-btn">
+        <img src="../images/searchIcon.svg" alt="Search icon" />
+      </button>
     </header>
   );
 }
