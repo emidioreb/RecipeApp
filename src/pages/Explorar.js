cf3fe87ad1919706c38b1a7ea7d45173aaa90bc5
Lombeira <glomenha@gmail.com>
2021-11-08 19:27:30 -0300

    fixing overall issues

--- src/pages/Explorar.js
+++ src/pages/Explorar.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Header from '../components/Header';
 import drinkIcon from '../images/drinkIcon.svg';
 import mealIcon from '../images/mealIcon.svg';
 
 export default function Explorar() {
   return (
     <div>
+      <Header title="Explorar" isVisible="none" />
       <Link to="/explorar/bebidas">
         <button type="button" data-testid="explore-food">
           <img src={ mealIcon } alt="ícone de Comidas" />
-          Explorar Comidas
+          Explorar Bebidas
         </button>
       </Link>
       <Link to="/explorar/comidas">
         <button type="button" data-testid="explore-drinks">
           <img src={ drinkIcon } alt="ícone de Bebidas" />
-          Explorar Bebidas
+          Explorar Comidas
         </button>
       </Link>
     </div>
