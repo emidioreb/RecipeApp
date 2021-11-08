a905013437e85db418ed0405705ea65cc46bbba5
Lombeira <glomenha@gmail.com>
2021-11-08 19:29:07 -0300

    Importing PropTypes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 import { useHistory } from 'react-router';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
