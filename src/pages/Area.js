6d2876f5c6836746fc3ba862934ac5ca6f9064ca
Leomar Linhares <leomar_linhares@hotmail.com>
2021-11-09 15:49:24 -0300

    impl: creates page Area

---
+++ src/pages/Area.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Header from '../components/Header';
+
+export default function Area() {
+  return (
+    <div>
+      <Header title="Explorar Origem" isVisible="none" />
+    </div>
+  );
+}
