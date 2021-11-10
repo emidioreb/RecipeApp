ef6e2520dadf2c272af79ec35fca9db1f169775a
Leomar Linhares <leomar_linhares@hotmail.com>
2021-11-10 18:33:44 -0300

    impl: add new features to useRecipeDetails

--- src/hooks/useRecipeDetails.js
+++ src/hooks/useRecipeDetails.js
@@ -2,22 +2,47 @@ import { useState, useEffect } from 'react';
 import { useLocation } from 'react-router';
 
 function useRecipeDetails(id) {
-  let URL = '';
   const { pathname } = useLocation();
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
   const [recipe, setRecipe] = useState({});
-  if (pathname.includes('comida')) {
-    URL = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`;
-  } else if (pathname.includes('bebida')) {
-    URL = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`;
+
+  function getTypeAndURLInfo() {
+    if (pathname.includes('comida')) {
+      return {
+        URL: `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`,
+        type: 'meals',
+        onlyType: 'Meal',
+      };
+    }
+    return {
+      URL: `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`,
+      type: 'drinks',
+      onlyType: 'Drink',
+    };
+  }
+
+  function treatVideoID(link) {
+    const VIDEO_ID_CHARACTERS_NUMBER = -11;
+    if (link) {
+      const embedID = link.slice(VIDEO_ID_CHARACTERS_NUMBER);
+      return embedID;
+    }
+    return undefined;
   }
 
   useEffect(() => {
-    setLoading(true);
+    const { URL, type, onlyType } = getTypeAndURLInfo();
     async function fetchData() {
       const response = await fetch(URL);
-      const { meals } = await response.json();
-      setRecipe(meals[0]);
+      const recipeObject = await response.json();
+      const data = {
+        image: recipeObject[type][0][`str${onlyType}Thumb`],
+        title: recipeObject[type][0][`str${onlyType}`],
+        category: recipeObject[type][0].strCategory,
+        instructions: recipeObject[type][0].strInstructions,
+        video: treatVideoID(recipeObject[type][0].strYoutube),
+      };
+      setRecipe(data);
     }
     fetchData();
     setLoading(false);
