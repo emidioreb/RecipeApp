804df334400819fc60112d76baacf14911e32b0d
Leomar Linhares <leomar_linhares@hotmail.com>
2021-11-18 17:03:45 -0300

    impl: add random route to link on food

--- src/pages/ExplorarComidas.js
+++ src/pages/ExplorarComidas.js
@@ -4,10 +4,12 @@ import { useHistory } from 'react-router';
 import Header from '../components/Header';
 import useIngredients from '../hooks/useIngredients';
 import Footer from '../components/Footer';
+import useRandomRecipe from '../hooks/useRandomRecipe';
 
 export default function ExplorarComidas() {
   const { setIngredientRequestURL, setType } = useIngredients();
   const history = useHistory();
+  const randomID = useRandomRecipe();
   setIngredientRequestURL(
     'https://www.themealdb.com/api/json/v1/1/list.php?i=list',
   );
@@ -38,7 +40,7 @@ export default function ExplorarComidas() {
             Por Local de Origem
           </button>
         </Link>
-        <Link to="/comidas/:id-da-receita">
+        <Link to={ `/comidas/${randomID}` }>
           <button
             className="explore-btn"
             type="button"
